f944cd2f4bb5ddfae13929acda820b809b7e761f
Gerardo Gabriel <gmm.240699@gmail.com>
2023-03-21 13:28:35 -0600

    Added Hystrix dependencies and config

--- MicroServiceTest2/src/main/java/com/ms/test2/microservicetest2/controllers/ProductController.java
+++ MicroServiceTest2/src/main/java/com/ms/test2/microservicetest2/controllers/ProductController.java
@@ -16,7 +16,6 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/products")
